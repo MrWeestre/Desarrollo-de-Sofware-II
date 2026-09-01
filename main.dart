89b466c25c06530f477e44835654a81cd4608650
Israel Duran <023101380h@uandina.edu.pe>
2026-08-31 22:24:05 -0500

    Add files via upload
    Cambio del archivo mai para dar inicio a la guia3

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'pantalla_establecimiento.dart';
+import 'pantalla_personal.dart';
+import 'pantalla_oportunidades.dart';
+import 'oportunidad_con_error.dart';
+
+void main() {
+  runApp(const AppAuditoriaHigiene());
+}
+
+class AppAuditoriaHigiene extends StatelessWidget {
+  const AppAuditoriaHigiene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Auditoría de Higiene de Manos',
+      // Cambia la pantalla aquí para probar cada una:
+      // - PantallaEstablecimiento()
+      // - PantallaPersonal()
+      // - PantallaOportunidades()
+      home: const PantallaEstablecimiento(),
+    );
+  }
+}
