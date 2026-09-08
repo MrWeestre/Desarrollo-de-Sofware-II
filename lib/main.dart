d79dc577203861738023a8ea4c99b8739d4930a7
Israel Duran <023101380h@uandina.edu.pe>
2026-09-07 22:00:46 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'pantalla_bienvenida.dart';
+import 'tarjeta_momentos.dart';
+import 'pantalla_establecimiento.dart';
+import 'pantalla_personal.dart';
+import 'pantalla_oportunidades.dart';
 import 'oportunidad_con_error.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: Center(
-        child: oportunidadConError(),
-      ),
-    ),
-  ));
+  runApp(const AppAuditoriaHigiene());
+}
+
+class AppAuditoriaHigiene extends StatelessWidget {
+  const AppAuditoriaHigiene({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Auditoría de Higiene de Manos',
+      home: const PantallaBienvenida(),
+    );
+  }
 }
